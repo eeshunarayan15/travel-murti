b0a19ceba3bc75330bf1ea06d4944db5c0995637
Eeshu Narayan <eeshunarayan15@gmail.com>
2026-03-01 15:19:24 +0530

    removed latest change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ import WhyChooseUs from "@/components/WhyChooseUs/WhyChooseUs";
 const Home = () => {
   return (
     <div>
-      {/* <TripPlannerModal /> */}
+      <TripPlannerModal />
       <HeroSection />
       <DealOfTheDay />
       <SpiritualSubPackages />
