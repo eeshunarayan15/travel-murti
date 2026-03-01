6219e0844321e5b84bb8e778e13d0ff3801f295b
Eeshu Narayan <eeshunarayan15@gmail.com>
2026-03-01 15:37:24 +0530

    enabling the frequently asked questions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
       <Testimonials />
       {/* <RelatedBlogs/> */}
       <WhyChooseUs /> 
-      {/* <FrequentlyAskedQuestion /> */}
+      <FrequentlyAskedQuestion />
     </div>
   );
 }
